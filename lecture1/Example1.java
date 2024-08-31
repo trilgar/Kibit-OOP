e6c301611b450e9680b41643841e87f0e94b0583
zarytsko <zaritskiy.alexey@gmail.com>
2024-08-31 14:41:29 +0300

    Lecture 1

---
+++ lecture1/Example1.java
@@ -0,0 +1,42 @@
+// Клас для представлення клієнта
+class Customer {
+    String name;
+    String address;
+
+    // Метод для надсилання замовлення
+    String takeOrder(SalesPerson salesPerson, String sofaType, String deliveryDate) {
+        return salesPerson.processOrder(sofaType, deliveryDate);
+    }
+}
+
+// Клас для представлення продавця
+class SalesPerson {
+    String name;
+
+    // Метод для обробки замовлення
+    String processOrder(String sofaType, String deliveryDate) {
+        if (checkStock(sofaType) && checkDeliveryDate(deliveryDate)) {
+            deliverOrder(sofaType, deliveryDate);
+            return "Ваше замовлення виконано. Ми доставимо товар у " + deliveryDate;
+        } else {
+            return "Вибачте, ваш запит не може бути виконано.";
+        }
+    }
+
+    // Метод для перевірки запасу
+    boolean checkStock(String sofaType) {
+        // Логіка перевірки запасу
+        return true; // припустимо, що товар є на складі
+    }
+
+    // Метод для перевірки дати доставки
+    boolean checkDeliveryDate(String deliveryDate) {
+        // Логіка перевірки дати доставки
+        return true; // припустимо, що дата підходить
+    }
+
+    // Метод для доставки замовлення
+    void deliverOrder(String sofaType, String deliveryDate) {
+        // Логіка доставки товару
+    }
+}
