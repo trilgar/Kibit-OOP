0239e2e5c3d6b189c3a64ec85c1a69154afd1f93
zarytsko <zaritskiy.alexey@gmail.com>
2024-09-05 18:54:32 +0300

    Lectute 2

--- lecture2/example5/Animals.java
+++ lecture2/example5/Animals.java
@@ -2,32 +2,120 @@
 package lecture2.example5;
 
 abstract class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Абстрактний метод, який буде реалізований у підкласах
+    public abstract void makeSound();
 }
 
 abstract class AnimalWithBackbone extends Animal {
+    protected boolean hasBackbone = true;
+
+    public AnimalWithBackbone(String name, int age) {
+        super(name, age);
+    }
+
+    public boolean hasBackbone() {
+        return hasBackbone;
+    }
 }
 
 abstract class AnimalWithoutBackbone extends Animal {
+    protected boolean hasBackbone = false;
+
+    public AnimalWithoutBackbone(String name, int age) {
+        super(name, age);
+    }
+
+    public boolean hasBackbone() {
+        return hasBackbone;
+    }
 }
 
 // конкретні класи
 
 class Mammal extends AnimalWithBackbone {
-    public Mammal() {
+    private String furColor;
+
+    public Mammal(String name, int age, String furColor) {
+        super(name, age);
+        this.furColor = furColor;
+    }
+
+    public String getFurColor() {
+        return furColor;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: I'm a mammal!");
     }
 }
 
 class Fish extends AnimalWithBackbone {
-    public Fish() {
+    private boolean isSaltwater;
+
+    public Fish(String name, int age, boolean isSaltwater) {
+        super(name, age);
+        this.isSaltwater = isSaltwater;
+    }
+
+    public boolean isSaltwater() {
+        return isSaltwater;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Blub blub!");
     }
 }
 
 class Amphibian extends AnimalWithBackbone {
-    public Amphibian() {
+    private String skinType;
+
+    public Amphibian(String name, int age, String skinType) {
+        super(name, age);
+        this.skinType = skinType;
+    }
+
+    public String getSkinType() {
+        return skinType;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Croak croak!");
     }
 }
 
 class Insect extends AnimalWithoutBackbone {
-    public Insect() {
+    private int numberOfLegs;
+
+    public Insect(String name, int age, int numberOfLegs) {
+        super(name, age);
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    public int getNumberOfLegs() {
+        return numberOfLegs;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Buzz buzz!");
     }
 }
--- lecture2/example5/Main.java
+++ lecture2/example5/Main.java
@@ -2,11 +2,24 @@ package lecture2.example5;
 
 public class Main {
     public static void main(String[] args) {
-        // Створюємо об'єкти конкретних класів класів
-        Mammal mammal = new Mammal();
-        Fish fish = new Fish();
-        Amphibian amphibian = new Amphibian();
-        Insect insect = new Insect();
+        // Створюємо об'єкти конкретних класів
+        Mammal mammal = new Mammal("Elephant", 10, "Gray");
+        Fish fish = new Fish("Shark", 5, true);
+        Amphibian amphibian = new Amphibian("Frog", 2, "Moist");
+        Insect insect = new Insect("Beetle", 1, 6);
+
+        // Викликаємо методи для кожного об'єкта
+        System.out.println(mammal.getName() + " is " + mammal.getAge() + " years old and has " + mammal.getFurColor() + " fur.");
+        mammal.makeSound();
+
+        System.out.println(fish.getName() + " is " + fish.getAge() + " years old and is " + (fish.isSaltwater() ? "a saltwater" : "a freshwater") + " fish.");
+        fish.makeSound();
+
+        System.out.println(amphibian.getName() + " is " + amphibian.getAge() + " years old and has " + amphibian.getSkinType() + " skin.");
+        amphibian.makeSound();
+
+        System.out.println(insect.getName() + " is " + insect.getAge() + " years old and has " + insect.getNumberOfLegs() + " legs.");
+        insect.makeSound();
 
         // Пробуємо створити об'єкт абстрактного класу
         // Animal animal = new Animal(); // Помилка компіляції
