2942ba07dcc57c320c3fbb52de807c392a1f9308
zarytsko <zaritskiy.alexey@gmail.com>
2024-09-15 16:40:18 +0300

    Lectute 3

---
+++ lecture4/example31/Main.java
@@ -0,0 +1,65 @@
+package lecture4.example31;
+
+public class Main {
+    public static void main(String argv[]) {
+        Manager m = new Manager("Simon", "01234M", 9000.0f, 2000.0f);
+        Secretary s = new Secretary("Selene", "98765S", 2500.0f);
+        System.out.print("The Manager " + m.getName() + " (employee number " + m.getEmployeeNumber() + ")");
+        System.out.println(" has a salary of " + m.getBasicSalary());
+        System.out.print("The Secretary " + s.getName() + "(employee number " + s.getEmployeeNumber() + ")");
+        System.out.println(" has a salary of " + s.getBasicSalary());
+        System.out.print("The Manager " + m.getName());
+        System.out.println(" also has an allowance of " + m.getAllowance());
+    }
+}
+
+class Person {
+    private String name;
+
+    Person(String aName) {
+        name = aName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Employee extends Person {
+    private float basicSalary;
+    private String employeeNumber;
+
+    Employee(String aName, String aEmployeeNumber, float aBasicSalary) {
+        super(aName);
+        employeeNumber = aEmployeeNumber;
+        basicSalary = aBasicSalary;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public float getBasicSalary() {
+        return basicSalary;
+    }
+}
+
+class Manager extends Employee {
+    private float allowance;
+
+    Manager(String aName, String aEmployeeNumber, float aBasicSalary, float aAllowanceAmt) {
+        super(aName, aEmployeeNumber, aBasicSalary);
+        allowance = aAllowanceAmt;
+    }
+
+    public float getAllowance() {
+        return allowance;
+    }
+}
+
+class Secretary extends Employee {
+    Secretary(String aName, String aEmployeeNumber,
+              float aBasicSalary) {
+        super(aName, aEmployeeNumber, aBasicSalary);
+    }
+}
